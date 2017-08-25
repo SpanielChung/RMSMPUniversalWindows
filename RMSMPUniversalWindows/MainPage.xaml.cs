1bd0455af961ffa12e04773bd46f085855000113
Dan Young <dan.young12@gmail.com>
2017-08-25 11:44:44 -0400

    init

---
+++ RMSMPUniversalWindows/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RMSMPUniversalWindows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // local variables
+        private SerialDevice serialPort = null;
+        DataWriter dataWriteObject = null;
+        DataReader dataReaderObject = null;
+        private string arduinoSerialName = "USB";
+        private CancellationTokenSource ReadCancellationTokenSource;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Start listening to serial
+            Listen();
+        }
+
+
+        private async void Listen()
+        {
+            await SerialDevice.FromIdAsync("");
+
+        }
+    }
+}
